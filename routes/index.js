1e10a7f4ff97bb310578775946f36efe927f1089
jwahrman22 <wahrman.jd@gmail.com>
2020-02-24 21:26:45 -0600

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+router.use('/script', express.static('public'))
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Todo App' });
+});
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+     name: 'JD',
+     hobbies: ['golf','fishing','biking']
+     });
 });
 
 module.exports = router;
